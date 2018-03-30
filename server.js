17fa5218a8760c97a11fc62995b06166c5ca3008
begimov <begimov@gmail.com>
2018-03-30 17:23:54 +0300

    server.js implemented

---
+++ server.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const express = require('express')
+const { createBundleRenderer } = require('vue-server-renderer')
+
+app = express()
+
+const bundle = require('./dist/vue-ssr-server-bundle.json')
+
+const renderer = createBundleRenderer(bundle, {
+    basedir: './dist',
+    template: fs.readFileSync('./index.html', 'utf-8'),
+    clientManifest: require('./dist/vue-ssr-client-manifest.json')
+})
+
+app.get('*', (req, res) => {
+    renderer.renderToString({
+        url: req.url
+    }, (err, html) => {
+        res.send(html)
+    })
+})
+
+app.listen(8080)
