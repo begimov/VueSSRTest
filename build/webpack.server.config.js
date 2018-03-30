41c0bf9f1b04f5feb812c23de8dc196919a6e785
begimov <begimov@gmail.com>
2018-03-30 16:59:20 +0300

    webpack server config updated

--- build/webpack.server.config.js
+++ build/webpack.server.config.js
@@ -2,7 +2,12 @@ const merge = require('webpack-merge')
 const base = require('./webpack.base.config')
 
 module.exports = merge(base, {
+    target: 'node',
     entry: {
         app: './src/main.server.js'
-    }
+    },
+    output: {
+        libraryTarget: 'commonjs2'
+    },
+    externals: nodeExternals()
 })
